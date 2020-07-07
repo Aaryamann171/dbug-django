1f2857eb980b696272bc5ada01328a743d2c9c65
Aaryamann <aaryamann171@gmail.com>
2020-07-07 09:50:12 +0530

    added review req table, made relevent changes in views

--- reviewer/models.py
+++ reviewer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ReviewRequest(models.Model):
+    code = models.TextField()
+    req_from = models.CharField(max_length=10)
+    req_to = models.CharField(max_length=10)
